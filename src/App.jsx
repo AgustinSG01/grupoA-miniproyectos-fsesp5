2b8e0ee0d321b64b952d9a088bd5d38c5648ec91
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-02-05 12:59:43 +0100

    Primer componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import TodosPage from "./pages/TodosPage";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="" element={<TodosPage/>}></Route>
+    </Routes>
+  );
 }
 
 export default App;
